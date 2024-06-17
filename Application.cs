e1951adba4da21ab1cc031517f4007662a35bd6b
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-06-16 21:08:18 -0300

    created Application File and MainWindow

---
+++ Application.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.UI;
+
+namespace ParamScannerAddIn
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    public class Application : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Views/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ParamScannerAddIn.Views.MainWindow
+{
+    #region Main Window View Scanner
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    } 
+    #endregion
+}
