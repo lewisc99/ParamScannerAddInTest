ad96c56941ffb6519105ccf236d3a27f8a2280e8
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-06-17 10:04:53 -0300

    created buttons  and layout to trigger parameters

--- Views/MainWindow/MainWindow.xaml.cs
+++ Views/MainWindow/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using ParamScannerAddIn.EventHandles;
+using ParamScannerAddIn.Utils;
+using System.Collections.Generic;
+using System;
 using System.Windows;
+using System.Linq;
 
 namespace ParamScannerAddIn.Views.MainWindow
 {
@@ -16,12 +20,66 @@ namespace ParamScannerAddIn.Views.MainWindow
         {
             _uiDoc = uiApp.ActiveUIDocument;
             _doc = _uiDoc.Document;
-            //Closed += MainWindow_Closed;
+            Closed += MainWindow_Closed;
 
             InitializeComponent();
             _mIsolateElementsHandler = mIsolateElementsHandler;
         }
 
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Isolate_In_View_Click(object sender, RoutedEventArgs e)
+        {
+            string paramName = txt_Parameter_name.Text;
+            string paramValue = txt_Parameter_Value.Text;
+
+            if (string.IsNullOrEmpty(txt_Parameter_name.Text))
+            {
+                MessageBox.Show("Parameter Name cannot be left empty");
+                return;
+            }
+
+            GetParameterByNameAndValue parameterFinder = new GetParameterByNameAndValue();
+            List<Element> elementsFound = parameterFinder.FindParameterByNameAndValue(_doc, paramName, paramValue);
+
+            if (elementsFound.Count == 0)
+            { return; }
+
+            _mIsolateElementsHandler.Uidoc = _uiDoc;
+
+            _mIsolateElementsHandler.Raise(elementsFound.Select(elementItem => elementItem.Id).ToList());
+
+            MessageBox.Show("Item Isolated");
+        }
+
+        private void btn_Select_Click(object sender, RoutedEventArgs e)
+        {
+
+            string paramName = txt_Parameter_name.Text;
+            string paramValue = txt_Parameter_Value.Text;
+
+            if (string.IsNullOrEmpty(txt_Parameter_name.Text))
+            {
+                MessageBox.Show("Parameter Name cannot be left empty");
+                return;
+            }
+
+            GetParameterByNameAndValue parameterFinder = new GetParameterByNameAndValue();
+            List<Element> elementsFound = parameterFinder.FindParameterByNameAndValue(_doc, paramName, paramValue);
+
+            if (elementsFound.Count == 0)
+            { return; }
+
+            foreach (Element matchingElement in elementsFound)
+            {
+                _uiDoc.Selection.SetElementIds(elementsFound.Select(foundElement => foundElement.Id).ToList());
+            }
+
+            MessageBox.Show("Itens Selected");
+        }
     } 
     #endregion
 }
